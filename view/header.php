970a82c1ec29eed10e0a16a29887ea0b08583f72
MaximeH2003 <Maximeh2003@gmail.com>
2022-03-09 17:30:27 +0100

    tamer

--- view/header.php
+++ view/header.php
@@ -1,18 +1,41 @@
-<?php
-/*
-if ( !isset($_SESSION['Panier']))
-    $_SESSION['Panier'] = new Panier;
-
-echo "<p> Nombre de produit dans le panier : " . $_SESSION['Panier']->getNombreProduits() . "</p>";
-echo "<p> Sous total panier : " . $_SESSION['Panier']->getSousTotal() . "</p>";
-*/
-echo "<a href = '". base() . "home'>Home</a>
-        <a href = '". base() . "Outils'>Outils</a>
-        <a href = '". base() . "vocabulaire'>Vocabulaire</a>
-        <a href = '". base() . "Commandes'>Commandes</a>";
-?>
-
-
-
-
-
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Phishing Site Ynav</title>
+        <?php
+            $onglet =  explode("/", $_SERVER['QUERY_STRING'])[0];
+            echo "<link rel=\"stylesheet\" href=\"" . base() . "CSS/style.css\" type=\"text/css\">";
+            echo "<link rel=\"stylesheet\" href=\"" . base() . "CSS/footer.css\" type=\"text/css\">";
+            echo "<link rel=\"stylesheet\" href=\"" . base() . "CSS/header.css\" type=\"text/css\">";
+        ?>
+	</head>
+<body>
+    
+     <header>
+		<ul>
+			<li>
+				<span>
+                    <?php
+                    echo("<a class ='float_left" . ($onglet == "Wiki Cybersécurité" ? "active" : "") . "' href = '". base() . "Wiki Cybersécurité'>Wiki Cybersécurité</a>");
+                    ?>            
+				</span>
+			</li>
+			<li>
+                <?php
+                    echo("<a class ='float_left" . ($onglet == "Commandes" ? "active" : "") . "' href = '". base() . "Commandes'>Commandes</a>");
+                ?>
+	    	</li>
+			<li>
+                <?php
+                echo("<a class ='float_left" . ($onglet == "vocabulaire" ? "active" : "") . "'href = '". base() . "vocabulaire'>vocabulaire</a>");
+                ?>	
+			</li>
+			<li>
+                <?php
+                    echo("<a class ='float_left" . ($onglet == "Outils" ? "active" : "") . "'href = '". base() . "Outils'>Outils</a>");
+                ?> 
+			</li>
+		</ul>
+    </header>
+	</body>
+</html>
