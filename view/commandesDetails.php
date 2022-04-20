3d11456f7b1e7b8533e4fecd38ee189c1a8bc9f8
Crofto <tanguyreymbaut@gmail.com>
2022-04-20 11:39:36 +0200

    Mise en forme debut

--- view/commandesDetails.php
+++ view/commandesDetails.php
@@ -1,14 +1,14 @@
 <?php
 
 //On boucle sur la liste d'outil 
-echo '<p Class="commandeNomDetail">' .  $commande->nom . '</p>';
+echo '<h1 Class="commandeNomDetail">' .  $commande->nom . '</h1>';
 echo '<p Class="commandeDescriptionDetail">' ;
 //on affiche sa description longue si il en a une
 if ($commande->descriptionLongue == null || $commande->descriptionLongue == "" ){
     echo  $commande->descriptionCourte; 
 }
 else{
-    echo  $commande->descriptionCourte; 
+    echo   "<i>" . $commande->descriptionCourte ."</i>"; 
     echo  "<br/>"; 
     echo  $commande->descriptionLongue; 
 }
--- view/outilsDetails.php
+++ view/outilsDetails.php
@@ -1,7 +1,7 @@
 <?php
 
 //On boucle sur la liste d'outil 
-echo '<p Class="outilNomDetail">' .  $outil->nom . '</p>';
+echo '<h1 Class="outilNomDetail">' .  $outil->nom . '</h1>';
 echo '<p Class="outilDescriptionDetail">' ;
 //on affiche sa description longue si il en a une
 if ($outil->descriptionLongue == null || $outil->descriptionLongue == "" )
@@ -11,8 +11,13 @@ else
 
 echo '</p>';
 
+echo "<H2 Class='outilLien' >Les liens utiles : </H2>";
+
+echo "<h3>Les Get Started : </h3>";
 echo '<p Class="outilLienStarted"> <a href="' . $outil->lienGetStarted . '">' .  $outil->lienGetStarted . '</a></p>';
+echo "<h3>Les liens Officiel : </h3>";
 echo '<p Class="outilLienOfficiel"> <a href="' . $outil->lienOfficiel . '">' .  $outil->lienOfficiel . '</a></p>';
+echo "<h3>Les liens de documentation : </h3>";
 echo '<p Class="outilLienDoc"> <a href="' . $outil->lienDoc . '">' .  $outil->lienDoc . '</a></p>';
 
 ?>
